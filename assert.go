39e23fe9616fe7c2fa4a99a8f05d5a813d09b020
adrianwit <adrianwit@gmail.com>
2018-02-28 10:16:47 -0800

    integration with unit test

---
+++ assert.go
@@ -0,0 +1,29 @@
+package assertly
+
+import (
+	"fmt"
+	"testing"
+)
+
+//AssertValues validates expected against actual data structure
+func AssertValues(t *testing.T, expected, actual interface{}, arguments ... interface{}) bool {
+	validation, err := Assert(expected, actual, NewDataPath("/"))
+	if err != nil {
+		if len(arguments) > 0 {
+			handleFailure(t, fmt.Sprint(arguments...))
+		}
+		handleFailure(t, err)
+		return false
+	}
+
+	if validation.FailedCount != 0 {
+		if len(arguments) > 0 {
+			handleFailure(t, arguments...)
+		}
+		for _, failure := range validation.Failures {
+			handleFailure(t, fmt.Sprintf("%v: %v" ,failure.Path, failure.Message))
+		}
+		return false
+	}
+	return true
+}
