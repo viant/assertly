b891dafedd056a94ee5c37662d21ead016a3cab3
adrianwit <adrianwit@gmail.com>
2018-03-07 20:06:29 -0800

    added leaf key method

--- path_test.go
+++ path_test.go
@@ -16,3 +16,4 @@ func TestDataPath_Path(t *testing.T) {
 	subPath := path.Key("f1").Index(1).Key("s1")
 	assert.Equal(t, "[root]:f1[1].s1", subPath.Path())
 }
+
--- validation_test.go
+++ validation_test.go
@@ -5,41 +5,6 @@ import (
 	"testing"
 )
 
-func TestFailure_Index(t *testing.T) {
-
-	{
-		var failure = NewFailure("","[/]:ad[12].we", "", nil, nil)
-		assert.EqualValues(t, 12, failure.Index())
-	}
-	{
-		var failure = NewFailure("","ad[12].we", "", nil, nil)
-		assert.EqualValues(t, -1, failure.Index())
-	}
-	{
-		var failure = NewFailure("",":ad[a].we", "", nil, nil)
-		assert.EqualValues(t, -1, failure.Index())
-	}
-	{
-		var failure = NewFailure("",":ad[].we", "", nil, nil)
-		assert.EqualValues(t, -1, failure.Index())
-	}
-}
-
-
-func TestFailure_MergeFrom(t *testing.T) {
-	var failure = NewFailure("","[/]:ad[12].we", "", nil, nil)
-	source := NewValidation()
-	source.PassedCount = 2
-	source.AddFailure(failure)
-	target := NewValidation()
-	target.AddFailure(failure)
-	target.PassedCount = 2
-	target.MergeFrom(source)
-	assert.EqualValues(t, 4, target.PassedCount)
-	assert.EqualValues(t, 2, target.FailedCount)
-	assert.EqualValues(t, 2, len(target.Failures))
-}
-
 
 
 func TestValidation_Report(t *testing.T) {
