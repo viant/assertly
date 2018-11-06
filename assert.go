d830620595a90c937c944bceadd8dbbd73914392
adrianwit <adrianwit@gmail.com>
2018-11-05 17:58:45 -0800

    added helper method AssertValuesWithContext

--- assert.go
+++ assert.go
@@ -9,21 +9,40 @@ import (
 func AssertValues(t *testing.T, expected, actual interface{}, arguments ...interface{}) bool {
 	validation, err := Assert(expected, actual, NewDataPath("/"))
 	if err != nil {
-		if len(arguments) > 0 {
-			handleFailure(t, fmt.Sprint(arguments...))
-		}
-		handleFailure(t, err)
-		return false
+		return handlerValidationError(t, err, arguments...)
 	}
-
 	if validation.FailedCount != 0 {
-		if len(arguments) > 0 {
-			handleFailure(t, arguments...)
-		}
-		for _, failure := range validation.Failures {
-			handleFailure(t, fmt.Sprintf("%v: %v", failure.Path, failure.Message))
-		}
-		return false
+		return handlerValidationFailures(t, validation, arguments...)
 	}
 	return true
+}
+
+//AssertValuesWithContext validates expected against actual data structure with context
+func AssertValuesWithContext(context *Context, t *testing.T, expected, actual interface{}, arguments ...interface{}) bool {
+	validation, err := AssertWithContext(expected, actual, NewDataPath("/"), context)
+	if err != nil {
+		return handlerValidationError(t, err, arguments...)
+	}
+	if validation.FailedCount != 0 {
+		return handlerValidationFailures(t, validation, arguments...)
+	}
+	return true
+}
+
+func handlerValidationError(t *testing.T, err error, arguments ...interface{}) bool {
+	if len(arguments) > 0 {
+		handleFailure(t, fmt.Sprint(arguments...))
+	}
+	handleFailure(t, err)
+	return false
+}
+
+func handlerValidationFailures(t *testing.T, validation *Validation, arguments ...interface{}) bool {
+	if len(arguments) > 0 {
+		handleFailure(t, arguments...)
+	}
+	for _, failure := range validation.Failures {
+		handleFailure(t, fmt.Sprintf("%v: %v", failure.Path, failure.Message))
+	}
+	return false
 }
